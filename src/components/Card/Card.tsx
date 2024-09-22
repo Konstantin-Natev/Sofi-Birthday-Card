9a4622d6975e9cf4ae48bf4efee7dfb88924fecc
Konstantin-Natev <konstantin@nolimit.software>
2024-09-23 00:15:45 +0300

    fix the card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,9 +1,6 @@
 import { Grid, Typography } from "@mui/material";
 import styles from "./card.module.css";
-import 'animate.css';
-import { useLayoutEffect, useRef, useState } from "react";
-import { gsap } from "gsap";
-import Wormhole from "../Wormhole/Wormhole";
+import firstPic from "../../../public/Birthday03.png";
 
 interface CardProps {
     isCardClicked: boolean;
@@ -11,56 +8,23 @@ interface CardProps {
 }
 
 export const Card = ({ isCardClicked, setIsCardClicked }: CardProps) => {
-    const [showWormhole, setShowWormhole] = useState(false);
-    const bg1 = useRef(null);
-    const bg2 = useRef(null);
-
     const handleOpenCard = () => {
         setIsCardClicked(true);
     }
 
-    useLayoutEffect(() => {
-        const ctx = gsap.context(() => {
-          const isMobile = window.innerWidth < 600;
-          const pinAnim = gsap.timeline({ paused: true })
-            .to(bg1.current, { transform: isMobile ? "scale(1.5)" : "scale(3.05)", duration: isMobile ? 1 : 3, ease: "power2.out" });
-
-            if (isCardClicked) {
-                pinAnim.play();
-
-                setTimeout(() => {
-                    setShowWormhole(true);
-                }, 600);
-            } else {
-                pinAnim.reverse();
-                setShowWormhole(false);
-            }
-        });
-
-        return () => ctx.revert();
-    }, [isCardClicked]);
-
     return (
-        <>
-            <Grid className={styles.textContainer}>
-              <p className={styles.neonText}>Keep calm and click me</p>
+        <Grid className={`${styles.card} ${styles.rootContainer}`}>
+            <Grid className={`${styles.backSide} `}>
+               <Typography className={` ${styles.textContentCard}`}>Честит рожден ден, Софи! Желаем ти безкрайна радост, здраве и много любов. Нека всеки ден бъде изпълнен с усмивки и щастливи моменти, а сърцето ти бъде топло от любов и подкрепа. Нека годината ти донесе всичко, което желаеш!</Typography>
             </Grid>
-            <Grid ref={bg1} className={`${styles.card} ${styles.rootContainer} perspective animate__animated animate__zoomIn animate__delay-2s`}>
-                <Grid ref={bg2} className={`${styles.backSide} ${isCardClicked ? styles.backSideClicked : ""}`}>
-                </Grid>
-                <Grid onClick={handleOpenCard} className={`${styles.frontSide} ${isCardClicked ? styles.frontSideClicked : ""}`}>
-                    <Grid className={styles.contentContainer}>
-                        <Typography className={`${styles.headerText} ${styles.textContent}`}>Happy Birthday </Typography>
-                        <Typography className={styles.textContent}>Your first digital birthday card</Typography>
-                        <Typography className={styles.textContent}>Hand coded by scratch</Typography>
-                    </Grid>
+            <Grid onClick={handleOpenCard} className={`${styles.frontSide} ${isCardClicked ? styles.frontSideClicked : ""}`}>
+                <Grid className={styles.contentContainer}>
+                    <Typography className={`${styles.headerText} ${styles.textContent}`}>Happy Birthday Sofi</Typography>
+                    <Typography className={styles.textContent}>Your first digital birthday card</Typography>
+                    <Typography className={styles.textContent}>Hand coded by scratch</Typography>
+                    <img className={styles.birthdayCakeImage} src={firstPic} alt="" />
                 </Grid>
             </Grid>
-                    {showWormhole && (
-                        <div className={styles.wormholeContainer}>
-                            <Wormhole />
-                        </div>
-                    )}
-        </>
+        </Grid>
     );
 };
